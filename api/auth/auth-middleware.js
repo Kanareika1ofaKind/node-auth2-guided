fdf21fb2ee3d98fb645d7645276455f57981c1ac
Sydney <proteimus@gmail.com>
2025-07-22 14:02:31 -0500

    JsonWebToken completed

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,11 +1,31 @@
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET } = require('../../config')
+
 // AUTHENTICATION
 const restricted = (req, res, next) => {
+    const token = req.headers.authorization
+    if (!token) {
+        return next({ status: 401, message: 'What? no Token...' })
+    }
+    jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+        if (err) {
+            return next({ status: 401, message: 'Token invalid' })
+        }
+        req.decodedToken = decodedToken
+    })
   next()
 }
 
 // AUTHORIZATION
-const checkRole = (req, res, next) => {
-  next()
+const checkRole = role => (req, res, next) => {
+    //checkRole("admin")
+
+    if (req.decodedToken && req.decodedToken.role === role) {
+        next()
+    } else {
+        next({ status: 403, message: 'you have no power here!' })
+    }
+
 }
 
 module.exports = {
